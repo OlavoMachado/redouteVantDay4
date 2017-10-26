06153de52fa547824808d7da2cdf3f1f0ae6a58b
dev <dev@DevVM-eclipsebase>
2017-10-26 09:44:00 +0100

    com a classe ValidarLogin

---
+++ src/pkg15_gitproj/ValidarLogin.java
@@ -0,0 +1,16 @@
+package pkg15_gitproj;
+
+public class ValidarLogin {
+    
+    String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+}
